defcc65f672cc34becaaf2131e677888d7993b48
14likhit <14likhit@gmail.com>
2024-10-21 17:57:56 +0530

    [FEAT]: added location helper.

---
+++ app/src/main/java/com/fttx/partner/ui/utils/location/LastLocation.kt
@@ -0,0 +1,37 @@
+package com.fttx.partner.ui.utils.location
+
+import android.annotation.SuppressLint
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+
+/**
+ * Retrieves the last known user location asynchronously.
+ *
+ * @param onGetLastLocationSuccess Callback function invoked when the location is successfully retrieved.
+ *        It provides a Pair representing latitude and longitude.
+ * @param onGetLastLocationFailed Callback function invoked when an error occurs while retrieving the location.
+ *        It provides the Exception that occurred.
+ */
+@SuppressLint("MissingPermission")
+private fun getLastLocation(
+    context: Context,
+    fusedLocationProviderClient: FusedLocationProviderClient,
+    onGetLastLocationSuccess: (Pair<Double, Double>) -> Unit,
+    onGetLastLocationFailed: (Exception) -> Unit
+) {
+    // Check if location permissions are granted
+    if (areLocationPermissionsGranted(context)) {
+        // Retrieve the last known location
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener { location ->
+                location?.let {
+                    // If location is not null, invoke the success callback with latitude and longitude
+                    onGetLastLocationSuccess(Pair(it.latitude, it.longitude))
+                }
+            }
+            .addOnFailureListener { exception ->
+                // If an error occurs, invoke the failure callback with the exception
+                onGetLastLocationFailed(exception)
+            }
+    }
+}
